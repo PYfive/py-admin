f5b782769815ab9a93c038da02a510a39f95d36c
mst <100874147@qq.com>
2021-03-29 22:31:31 +0800

    day 3

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,33 +2,31 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
 
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost:3306/test_db'
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost:3306/test_db'
 db = SQLAlchemy(app)
 
-if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
 
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255), unique=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 # a simple page that says hello
@@ -39,7 +37,6 @@ def hello():
 
 @app.route('/login', methods=['POST'])
 def login():
-    return 'Hello, login!'
-
-
-return app
+    objs = db.session.query(User.username, User.password)
+    data = request.get_json
+    return objs
